3b3df827ba49dfbe4bf092f04ecf01259fa7dce8
aveiro7 <o.rosik.rosinska@gmail.com>
2017-04-27 20:13:19 +0200

    added js script to project

---
+++ project/theme-changer.js
@@ -0,0 +1,7 @@
+document.getElementById("light").addEventListener("click", function (event) {
+   document.body.className = "light";
+});
+
+document.getElementById("dark").addEventListener("click", function (event) {
+   document.body.className = "dark";
+});
