c8b74d6ca22d69cb281016d70eb437a38ab40d15
ola <o.rosik.rosinska@gmail.com>
2018-03-17 21:04:14 +0100

    added exercises solutions

---
+++ exercises/buttons.js
@@ -0,0 +1,7 @@
+document.getElementById("button1").addEventListener("click", function(event) {
+	document.body.className = "light-theme";
+});
+
+document.getElementById("button2").addEventListener("click", function (event) {
+	document.body.className = "dark-theme";
+});
